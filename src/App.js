9218430d589c4b080bed7bc1a14cfb1798264b82
QueenterJuma <jumahqueenter@gmail.com>
2023-04-05 12:24:04 +0300

    Add update to Categories file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import Layout from './components/Layout';
 import Categories from './components/Categories';
-import Books from './components/Books';
+import BookHolder from './components/BookHolder';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<BookHolder />} />
+      <Route path="categories" element={<Categories />} />
+    </Route>,
+  ),
+);
 
 function App() {
   return (
     <div className="App">
-      <div className="home">
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="categories" element={<Categories />} />
-        </Routes>
-      </div>
+      <RouterProvider router={router} />
     </div>
   );
 }
